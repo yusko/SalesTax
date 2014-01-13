6505700c98ab8709547f49426612253eb921c5ba
yusko <yusko@me.com>
2014-01-12 21:52:49 -0500

    1/12/2013 @ 9:52 - Initial
    default blank project

---
+++ SalesTax/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesTax
+{
+    class Product
+    {
+        public enum ProductType
+        {
+            BOOK,
+            MEDICAL,
+            FOOD
+        }
+
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public bool localtax { get; set; }
+        public bool imported { get; set; }
+    }
+}
