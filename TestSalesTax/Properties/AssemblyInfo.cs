83823f56282481fb402bf1f60c4cddc2a19488d7
yusko <yusko@me.com>
2014-01-16 14:00:42 -0500

    1/16/2014 @ 2:00PM
    added test files and nunit test classes.

--- TestSalesTax/Properties/AssemblyInfo.cs
+++ TestSalesTax/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SalesTax")]
+[assembly: AssemblyTitle("TestSalesTax")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SalesTax")]
+[assembly: AssemblyProduct("TestSalesTax")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("050f4295-21d4-42e9-86e1-64d5612071e1")]
+[assembly: Guid("31ec9588-be5c-4ba9-928a-9d2faf61c8cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestSalesTax")]
